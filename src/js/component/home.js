f6805b29e4363b12d6e8259f26b50238c36c5b11
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-03-14 03:26:51 +0000

    prueba

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,18 +5,14 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	let [frases, setFrase] = useState([
-		"Wash the dishes",
-		"Clean my room",
-		"Cook"
-	]);
+	let [frases, setFrase] = useState([]);
 
 	let [tarea, setTarea] = useState("");
 
 	const updatedtoDo = frases.map((listItems, i) => {
 		return (
 			<li className="ul" key={i} onClick={() => deleteToDo(i)}>
-				{listItems}
+				{listItems.label}
 			</li>
 		);
 	});
@@ -24,9 +20,37 @@ export function Home() {
 
 	function handleChange(k) {
 		if (k.keyCode === 13) {
-			setFrase([...frases, tarea]);
-			console.log(tarea);
+			setFrase([...frases, { label: tarea, done: false }]);
+			console.log(frases);
+			actualizar(frases);
 		}
+	}
+	function actualizar(frases) {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(frases);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/cris010411",
+			requestOptions
+		)
+			.then(response => response.text())
+			.then(result =>
+				setTarea(
+					result.map(item => {
+						return { label: item.label, done: item.done };
+					})
+				)
+			)
+			.catch(error => console.log("error", error));
 	}
 	function deleteToDo(i) {
 		let borrar = frases.filter(item => item !== frases[i]);
